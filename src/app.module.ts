e25a20e3e9232ff37a5dd0b8c60a60a976ccb280
impossible98 <christoleavenow@outlook.com>
2024-07-28 21:45:49 +0000

    feat: version 0.0.13

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,29 @@
+// import third-party modules
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+// import local modules
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { User } from './users/entities/user.entity';
+import {
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
+} from './constant';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'root',
-      password: 'root',
-      database: 'nestjs',
+      host: DATABASE_HOST,
+      port: DATABASE_PORT,
+      username: DATABASE_USER,
+      password: DATABASE_PASSWORD,
+      database: DATABASE_NAME,
       entities: [User],
     }),
     AuthModule,
--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,19 @@
+// import third-party modules
+import dotenv from 'dotenv';
+
 export const APP_NAME: string = 'Immich';
-export const APP_VERSION: string = '0.0.12';
+export const APP_VERSION: string = '0.0.13';
 export const PORT: number = parseInt(process.env.PORT, 10) || 3000;
+
+if (process.env.NODE_ENV == 'production') {
+  dotenv.config();
+} else {
+  dotenv.config({ path: '.env.local' });
+}
+
+export const DATABASE_HOST: string = process.env.DATABASE_HOST;
+export const DATABASE_PORT: number =
+  parseInt(process.env.DATABASE_PORT, 10) || 5432;
+export const DATABASE_USER: string = process.env.DATABASE_USER;
+export const DATABASE_PASSWORD: string = process.env.DATABASE_PASSWORD;
+export const DATABASE_NAME: string = process.env.DATABASE_NAME;
