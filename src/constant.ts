ba375af3cf881d17fe332639a37212c4c18230ec
impossible98 <christoleavenow@outlook.com>
2024-07-28 19:56:12 +0000

    feat: version 0.0.10

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,3 @@
 export const APP_NAME: string = 'Immich';
-export const APP_VERSION: string = '0.0.9';
+export const APP_VERSION: string = '0.0.10';
 export const PORT: number = parseInt(process.env.PORT, 10) || 3000;
